f4e5ef3f88ec3df51f0de13db1eecd4de5ebd042
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-10-30 19:16:42 +0100

    feat(backend/middleware): add prettyJSON
    formats all json responses before returning them

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,17 +2,21 @@ import { Hono } from "hono";
 import type { hc } from "hono/client";
 import { HTTPException } from "hono/http-exception";
 import { logger } from "hono/logger";
+import { prettyJSON } from "hono/pretty-json";
 
 const app = new Hono();
 
-app.use(logger()).onError(async (error) => {
-	if (!(error instanceof HTTPException))
-		return new Response(error.message, {
-			status: 500,
-			statusText: `Internal error: ${error.cause}`,
-		});
-	return error.getResponse();
-});
+app
+	.use(logger())
+	.use(prettyJSON({ space: 2 }))
+	.onError(async (error) => {
+		if (!(error instanceof HTTPException))
+			return new Response(error.message, {
+				status: 500,
+				statusText: `Internal error: ${error.cause}`,
+			});
+		return error.getResponse();
+	});
 
 const routes = app.basePath("/api").get("/", (c) => {
 	return c.text("Hello Hono!");
