59f6e10f96fb4cbf6c6eff33a92349359d73e2b8
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-10-30 19:12:04 +0100

    feat(backend/middleware): add logging

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,7 +1,10 @@
 import { Hono } from "hono";
 import type { hc } from "hono/client";
+import { logger } from "hono/logger";
 
 const app = new Hono();
+
+app.use(logger());
 
 const routes = app.basePath("/api").get("/", (c) => {
 	return c.text("Hello Hono!");
