80cd49deab32ae65a63972f215f34403f42b26c2
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-10-30 16:38:46 +0100

    chore: add commitlint

---
+++ commitlint.config.ts
@@ -0,0 +1,7 @@
+const Configuration = {
+	extends: ["@commitlint/config-conventional"],
+	helpUrl:
+		"https://github.com/conventional-changelog/commitlint/#what-is-commitlint",
+};
+
+export default Configuration;
