5e40bb14f68812807924db0670379c730c8068ab
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-10-30 19:14:10 +0100

    feat(backend/middleware): add error middlware
    takes an error thrown by a handler and returns a fitting response

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,18 @@
 import { Hono } from "hono";
 import type { hc } from "hono/client";
+import { HTTPException } from "hono/http-exception";
 import { logger } from "hono/logger";
 
 const app = new Hono();
 
-app.use(logger());
+app.use(logger()).onError(async (error) => {
+	if (!(error instanceof HTTPException))
+		return new Response(error.message, {
+			status: 500,
+			statusText: `Internal error: ${error.cause}`,
+		});
+	return error.getResponse();
+});
 
 const routes = app.basePath("/api").get("/", (c) => {
 	return c.text("Hello Hono!");
