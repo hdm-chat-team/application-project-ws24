fc3eda42cd17a00180816d89e49eeda852eb9250
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-11-05 01:14:46 +0100

    feat(frontend): 📱 PWA
    added vite-pwa and assets generator plugins and enable PWA

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,10 +1,56 @@
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), TanStackRouterVite()],
+	plugins: [
+		react(),
+		TanStackRouterVite(),
+		VitePWA({
+			registerType: "autoUpdate",
+			devOptions: {
+				enabled: true,
+			},
+			includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+			manifest: {
+				name: "My Awesome App",
+				short_name: "MyApp",
+				description: "My Awesome App description",
+				theme_color: "#ffffff",
+				icons: [
+					{
+						src: "pwa-64x64.png",
+						sizes: "64x64",
+						type: "image/png",
+					},
+					{
+						src: "pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any",
+					},
+					{
+						src: "maskable-icon-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "maskable",
+					},
+				],
+			},
+		}),
+	],
 	server: {
 		proxy: {
 			"/api": "http://localhost:3000",
