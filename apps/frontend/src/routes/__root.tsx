168812f717c5be66249b8f0a0cca8216b3c842fc
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-11-05 21:21:04 +0100

    refactor(frontend): 🎨 router dev tools dev mode only

--- apps/frontend/src/routes/__root.tsx
+++ apps/frontend/src/routes/__root.tsx
@@ -1,6 +1,16 @@
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { Suspense, lazy } from "react";
+
+const TanStackRouterDevtools =
+	process.env.NODE_ENV === "production"
+		? () => null // Render nothing in production
+		: lazy(() =>
+				// Lazy load in development
+				import("@tanstack/router-devtools").then((res) => ({
+					default: res.TanStackRouterDevtools,
+				})),
+			);
 
 export const Route = createRootRoute({
 	component: () => (
