6f26d6b80abe01ba07a97fb1b08ae5473d70aae0
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-11-09 15:49:57 +0100

    refactor(backend/test): ✅ update test description

--- apps/backend/src/routes/rest/index.test.ts
+++ apps/backend/src/routes/rest/index.test.ts
@@ -1,11 +1,13 @@
-import { expect, test } from "bun:test";
+import { expect, test, describe } from "bun:test";
 import { testClient } from "hono/testing";
 import app from "../../app";
 import type { ClientType } from "../../types";
 
 const { api } = testClient(app) as ClientType;
 
-test("GET /", async () => {
-	const res = await api.rest.$get();
-	expect(res.ok).toBe(true);
+describe("rest", () => {
+	test("GET /", async () => {
+		const res = await api.rest.$get();
+		expect(res.ok).toBe(true);
+	});
 });
