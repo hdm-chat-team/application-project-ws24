c8b9a3bcd2ed49025285a6d0ebb450d4fe8e9152
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-11-10 15:39:20 +0100

    refactor(backend): 🔊 log  handler errors to console

--- apps/backend/src/lib/middleware.ts
+++ apps/backend/src/lib/middleware.ts
@@ -1,6 +1,7 @@
 import { HTTPException } from "hono/http-exception";
 
 export const errorHandler = async (error: Error) => {
+	console.error(error);
 	if (!(error instanceof HTTPException)) {
 		return new Response(error.message, {
 			status: 500,
