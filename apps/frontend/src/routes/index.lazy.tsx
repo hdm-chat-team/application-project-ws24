fb2795e38dca0a1903b25fe3c668ae75a2d748e6
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-11-10 16:02:14 +0100

    refactor(frontend): 🎨 socket in useEffect
    This is just React best practice

--- apps/frontend/src/routes/index.lazy.tsx
+++ apps/frontend/src/routes/index.lazy.tsx
@@ -1,17 +1,24 @@
 import api from "@/lib/api";
 import { useQuery } from "@tanstack/react-query";
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { useEffect } from "react";
 
 export const Route = createLazyFileRoute("/")({
 	component: Index,
 });
 
 function Index() {
-	const ws = api.ws.$ws();
+	useEffect(() => {
+		const socket = api.ws.$ws();
 
-	ws.addEventListener("open", () => {
-		ws.send("Hello from client!");
-	});
+		socket.onopen = (event) => {
+			console.log("WebSocket client opened", event);
+		};
+
+		return () => {
+			socket.close();
+		};
+	}, []);
 
 	const { data, isPending } = useQuery({
 		queryKey: ["data"],
