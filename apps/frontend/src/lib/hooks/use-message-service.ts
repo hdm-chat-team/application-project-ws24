66402076c7bbb4d616e57b283e28fa48def3e8b6
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-11-17 20:49:32 +0100

    style(frontend): 🎨 formatting
    Signed-off-by: Deniz Gazitepe <dg102@hdm-stuttgart.de>

--- apps/frontend/src/lib/hooks/use-message-service.ts
+++ apps/frontend/src/lib/hooks/use-message-service.ts
@@ -1,17 +1,23 @@
-import { useCallback } from 'react';
-import { MessageService } from '../message-service';
+import { useCallback } from "react";
+import { MessageService } from "../message-service";
 
 // * Create a custom hook to encapsulate the access to the MessageService
 export const useMessageService = () => {
-    const messageService = MessageService.getInstance();
+	const messageService = MessageService.getInstance();
 
-    const addMessage = useCallback(async (content: string) => {
-        await messageService.addMessage(content);
-    }, [messageService]);
+	const addMessage = useCallback(
+		async (content: string) => {
+			await messageService.addSentMessage(content);
+		},
+		[messageService],
+	);
 
-    const addReceivedMessage = useCallback(async (content: string) => {
-        await messageService.addReceivedMessage(content);
-    }, [messageService]);
+	const addReceivedMessage = useCallback(
+		async (content: string) => {
+			await messageService.addReceivedMessage(content);
+		},
+		[messageService],
+	);
 
-    return { addMessage, addReceivedMessage };
+	return { addMessage, addReceivedMessage };
 };
