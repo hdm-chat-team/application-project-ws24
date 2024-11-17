90ec6114465a86110764f2360794b886f5c01b7e
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-11-17 20:44:58 +0100

    style(frontend): 🎨 styling
    Signed-off-by: Deniz Gazitepe <dg102@hdm-stuttgart.de>

--- apps/frontend/src/lib/message-service.ts
+++ apps/frontend/src/lib/message-service.ts
@@ -1,72 +1,63 @@
 import Dexie from "dexie";
 import type { EntityTable } from "dexie";
 
-// * Define the message itself
 export interface Message {
-    id?: string;
-    content: string;
-    timestamp: number;
-    status: 'sent' | 'received';
+	id?: string;
+	content: string;
+	timestamp: number;
+	status: "sent" | "received";
 }
 
-// * Define the local database with the message table
-const messageDb = new Dexie('MessageDatabase') as Dexie & {
-    messages: EntityTable<Message, 'id'>
+const messageDb = new Dexie("MessageDatabase") as Dexie & {
+	messages: EntityTable<Message, "id">;
 };
 
-// * Define the version of the database and the table
 messageDb.version(1).stores({
-    messages: "++id, content, timestamp, type"
+	messages: "++id, content, timestamp, type",
 });
 
-// * Singleton MessageService to interact with the local database
 class MessageService {
-    private static instance: MessageService;
+	private static instance: MessageService;
 
-    private constructor() {}
+	private constructor() {}
 
-    public static getInstance(): MessageService {
-        if (!MessageService.instance) {
-            MessageService.instance = new MessageService();
-        }
-        return MessageService.instance;
-    }
+	public static getInstance() {
+		if (!MessageService.instance) {
+			MessageService.instance = new MessageService();
+		}
+		return MessageService.instance;
+	}
 
-    // * Helper function to add a message to the local database
-    private async addMessageToDb(message: Message): Promise<void> {
-        try {
-            await messageDb.table('messages').add(message);
-        } catch (error) {
-            console.error('Error adding message to database', error);
-        }
-    }
+	private async addMessageToDb(message: Message) {
+		try {
+			await messageDb.table("messages").add(message);
+		} catch (error) {
+			console.error("Error adding message to database", error);
+		}
+	}
 
-    // * Create a message object
-    private createMessage(content: string, status: 'sent' | 'received'): Message {
-        return {
+	private createMessage(content: string, status: "sent" | "received"): Message {
+		return {
 			id: crypto.randomUUID(),
-            content,
-            status,
-            timestamp: Date.now()
-        };
-    }
+			content,
+			status,
+			timestamp: Date.now(),
+		};
+	}
 
-    // * Add a message to the local database
-    public async addMessage(message: Message): Promise<void> {
-        await this.addMessageToDb(message);
-    }
+	public async addMessage(message: Message) {
+		await this.addMessageToDb(message);
+	}
 
-    // * Add a sent message to the local database
-    public async addSentMessage(content: string): Promise<void> {
-        const message = this.createMessage(content, 'sent');
-        await this.addMessage(message);
-    }
+	public async addSentMessage(content: string) {
+		const message = this.createMessage(content, "sent");
+		await this.addMessage(message);
+	}
 
-    // * Add a received message to the local database
-    public async addReceivedMessage(content: string): Promise<void> {
-        const message = this.createMessage(content, 'received');
-        await this.addMessage(message);
-    }
+	public async addReceivedMessage(content: string) {
+		const message = this.createMessage(content, "received");
+		await this.addMessage(message);
+	}
 }
 
 export { messageDb, MessageService };
