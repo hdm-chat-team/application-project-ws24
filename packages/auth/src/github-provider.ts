8c210b91c2f84b469b733b189d2bd73919b526d9
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-11-18 15:44:07 +0100

    refactor(auth): 🔥 remove console.log
    Signed-off-by: Deniz Gazitepe <dg102@hdm-stuttgart.de>

--- packages/auth/src/github-provider.ts
+++ packages/auth/src/github-provider.ts
@@ -1,10 +1,10 @@
 import { GitHub } from "arctic";
 
 const { CLIENT_ID_GITHUB, CLIENT_SECRET_GITHUB } = process.env;
-console.dir(process.env);
-if (!CLIENT_ID_GITHUB|| !CLIENT_SECRET_GITHUB) {
+if (!CLIENT_ID_GITHUB || !CLIENT_SECRET_GITHUB) {
 	throw new Error("GitHub ClientId and ClientSecret must be set");
 }
+
 export const github = new GitHub(
 	CLIENT_ID_GITHUB,
 	CLIENT_SECRET_GITHUB,
