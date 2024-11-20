ed5c755c14a49679c2d916fb28b0484fe3b5aa6d
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-11-20 14:27:47 +0100

    fix(frontend): 🩹 fix url
    Signed-off-by: Deniz Gazitepe <dg102@hdm-stuttgart.de>

--- app/frontend/src/lib/api.ts
+++ app/frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import type { ApiType } from "@server/client";
 import { hc } from "hono/client";
 
-const { api } = hc<ApiType>(window.location.origin);
+const { api } = hc<ApiType>("http://127.0.0.1:3000");
 
 export default api;
