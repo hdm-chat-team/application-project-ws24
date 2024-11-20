163bbfdbb67f2c3dc20298f637570321dc9992a2
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-11-20 20:41:46 +0100

    feat(backend): ✨ add context to factory
    This makes sure   all our routers have our custom context
    
    Signed-off-by: Deniz Gazitepe <dg102@hdm-stuttgart.de>

--- app/backend/lib/factory.ts
+++ app/backend/lib/factory.ts
@@ -6,9 +6,10 @@ import { logger } from "hono/logger";
 import { prettyJSON } from "hono/pretty-json";
 import { requestId } from "hono/request-id";
 import { config } from "./config";
+import type { Context } from "./types";
 
 export function createRouter() {
-	return new Hono({
+	return new Hono<Context>({
 		strict: false,
 	});
 }
