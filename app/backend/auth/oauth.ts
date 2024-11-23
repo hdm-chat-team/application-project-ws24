2cd2c82d1139f765b19be8a6bd0b35324aa3e219
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-11-23 21:41:43 +0100

    refactor: 🚚 callback URL as env variable
    .env needs to be adjusted!
    
    Signed-off-by: Deniz Gazitepe <dg102@hdm-stuttgart.de>

--- app/backend/auth/oauth.ts
+++ app/backend/auth/oauth.ts
@@ -1,12 +1,14 @@
 import { GitHub } from "arctic";
 
-const { CLIENT_ID_GITHUB, CLIENT_SECRET_GITHUB } = process.env;
-if (!CLIENT_ID_GITHUB || !CLIENT_SECRET_GITHUB) {
+const { CLIENT_ID_GITHUB, CLIENT_SECRET_GITHUB, CALLBACK_URL_GITHUB } =
+	process.env;
+
+if (!(CLIENT_ID_GITHUB && CLIENT_SECRET_GITHUB && CALLBACK_URL_GITHUB)) {
 	throw new Error("GitHub ClientId and ClientSecret must be set");
 }
 
 export const github = new GitHub(
 	CLIENT_ID_GITHUB,
 	CLIENT_SECRET_GITHUB,
-	"http://localhost:5173/api/auth/github/callback",
+	CALLBACK_URL_GITHUB,
 );
