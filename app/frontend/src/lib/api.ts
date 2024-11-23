d4226e3b2609796a1bdccd0222c9fce5c8069648
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-11-23 21:48:57 +0100

    fix(backend): 🐛 wrong rpc baseUrl during prod
    Signed-off-by: Deniz Gazitepe <dg102@hdm-stuttgart.de>

--- app/frontend/src/lib/api.ts
+++ app/frontend/src/lib/api.ts
@@ -1,7 +1,11 @@
 import type { ApiType } from "@server/app";
 import { hc } from "hono/client";
 
-const { api } = hc<ApiType>("http://localhost:3000", {
+const { DEV } = import.meta.env;
+
+const baseUrl = DEV ? "http://localhost:3000" : `${window.location.origin}`;
+
+const { api } = hc<ApiType>(baseUrl, {
 	init: {
 		credentials: "include",
 	},
