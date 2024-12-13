d6f6206d7cf2a34ca7ec141f645afb55566e529c
basti <bs117@hdm-stuttgart.de>
2024-12-13 20:17:37 +0100

    feat: :sparkles: New API endpoints to get his userdata, to show userdata, and to edit userdata from yourself.
    Set upped a type.ts to set rules for the profile and defines the data. Implemented profiles.ts API Endpoints to get his current profile,to get a profile by id and to update a his profile.

---
+++ app/backend/routes/user/profiles.ts
@@ -0,0 +1,64 @@
+import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import { HTTPException } from "hono/http-exception";
+import { eq, sql } from "drizzle-orm";
+import db from "../../db/db";
+import { userProfileTable } from "../../db/schema.sql";
+import { profileEditSchema, GUIDParamSchema } from "./types";
+import { protectedRoute, limiter } from "../../lib/middleware";
+import type { User, Session } from "#db/schema.sql";
+
+//* Define the profile route
+
+const profileRoute = new Hono<{Variables: { user: User; session: Session };}>();
+
+profileRoute
+	.use("*", limiter)
+
+	//* Endpoint to get the current user's profile
+	// * Protected route to ensure the user is authenticated
+	.get("/me", protectedRoute, async (c) => {
+		const user = c.get("user");
+		const data = await getUserProfile.execute({ id: user.id });
+		if (!data) {
+			throw new HTTPException(404, { message: "profile not found" });
+		}
+			return c.json({ data });
+		})
+
+	//* Endpoint to get a profile by id
+	.get("/:id",protectedRoute,zValidator("param", GUIDParamSchema),async (c) => {
+			const { id } = c.req.valid("param");
+			const userData = await getUserProfile.execute({ id });
+			if (!userData) {
+				throw new HTTPException(404, { message: "profile not found" });
+			}
+			return c.json({ data: userData });
+		},
+	)
+
+	//* Endpoint to update the profile
+	.put("/me",protectedRoute,zValidator("json", profileEditSchema),async (c) => {
+			const user = c.get("user");
+			const profile = c.req.valid("json");
+			await updateUserProfile.execute({ id: user.id, ...profile });
+			const updatedProfile = await getUserProfile.execute({ id: user.id });
+			return c.json({ message: "profile updated", data: updatedProfile });
+		},
+	);
+
+//* Prepared statement for getting user profiles
+const getUserProfile = db.query.userProfileTable.findFirst({with: {owner: true,},
+		where: eq(userProfileTable.userId, sql.placeholder("id")),
+	})
+	.prepare("get_user_profile");
+
+const updateUserProfile = db.update(userProfileTable).set({
+	displayName: sql.placeholder("displayName") as unknown as string,
+	avatar_url: sql.placeholder("avatar_url") as unknown as string,
+})
+	.where(eq(userProfileTable.userId, sql.placeholder("id")))
+	.prepare("update_user_profile");
+
+export default profileRoute;
+export type ProfileRoute = typeof profileRoute;
---
+++ app/backend/routes/user/types.ts
@@ -0,0 +1,45 @@
+import { z } from "zod";
+import type { ServerWebSocket } from "bun";
+import type { Env as HonoEnv } from "hono";
+import type { Session, User } from "#db/schema.sql";
+
+// * Set of rules for the profile
+// * Defines how the data should be validated
+// * Set what are valid inputs and what fields are allowed
+
+export type ChatSocket = ServerWebSocket<{ user: string }>;
+
+export interface Env extends HonoEnv {
+	Variables: {
+		user: User | null;
+		session: Session | null;
+	};
+}
+
+export const GUIDParamSchema = z.object({
+	id: z.string().uuid(),
+});
+
+//* Schema for profile editing
+export const profileEditSchema = z.object({
+	displayName: z
+		.string()
+		.min(1, "Name should be at least 1 character long")
+		.max(20, "Name should be at most 20 characters long")
+		.optional(),
+	avatar_url: z.string().url("Please enter a valid URL").optional().nullable(),
+});
+
+export const profileResponseSchema = z.object({
+	// * Profile Informations to display
+
+	displayName: z.string().nullable(),
+	avatar_url: z.string().nullable(),
+
+	// * User Informations to display
+	username: z.string(),
+	email: z.string().email(),
+});
+
+export type ProfileEditData = z.infer<typeof profileEditSchema>;
+export type ProfileResponse = z.infer<typeof profileResponseSchema>;
