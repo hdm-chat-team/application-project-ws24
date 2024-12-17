c012e2ff1609566d2eae26c0498167f03fa5488a
basti <bs117@hdm-stuttgart.de>
2024-12-17 13:50:51 +0100

    style: :art: code feeback

--- app/backend/routes/user/profiles.ts
+++ app/backend/routes/user/profiles.ts
@@ -1,18 +1,22 @@
 import { zValidator } from "@hono/zod-validator";
 import { eq, sql } from "drizzle-orm";
 import { HTTPException } from "hono/http-exception";
-import db from "../../db/db";
-import { userProfileTable } from "../../db/schema.sql";
-import { createRouter } from "../../lib/factory";
-import { protectedRoute } from "../../lib/middleware";
-import { GUIDParamSchema, profileEditSchema } from "./types";
+import { z } from "zod";
+import db from "#db/db";
+import { selectUserProfileSchema, userProfileTable } from "#db/schema.sql";
+import { createRouter } from "#lib/factory";
+import { protectedRoute } from "#lib/middleware";
 
-//* Define the profile route
+const GUIDParamSchema = z.object({
+	id: z.string(),
+});
+
+const profileEditSchema = selectUserProfileSchema.pick({
+	displayName: true,
+	avatar_url: true,
+});
 
 const profileRouter = createRouter()
-
-	//* Endpoint to get the current user's profile
-	// * Protected route to ensure the user is authenticated
 	.get("/me", protectedRoute, async (c) => {
 		const user = c.get("user");
 		const data = await getUserProfile.execute({ id: user.id });
@@ -22,7 +26,6 @@ const profileRouter = createRouter()
 		return c.json(data);
 	})
 
-	//* Endpoint to get a profile by id
 	.get(
 		"/:id",
 		protectedRoute,
@@ -37,7 +40,6 @@ const profileRouter = createRouter()
 		},
 	)
 
-	//* Endpoint to update the profile
 	.put(
 		"/me",
 		protectedRoute,
@@ -58,7 +60,6 @@ const profileRouter = createRouter()
 		},
 	);
 
-//* Prepared statement for getting user profiles
 const getUserProfile = db.query.userProfileTable
 	.findFirst({
 		with: { owner: true },
