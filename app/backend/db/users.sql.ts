b4099972c6ab71554667b0888f8a9b446a63440c
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-12-21 22:38:59 +0100

    perf(db): 🗃️ add user Id index
    Signed-off-by: Deniz Gazitepe <dg102@hdm-stuttgart.de>

--- app/backend/db/users.sql.ts
+++ app/backend/db/users.sql.ts
@@ -2,37 +2,50 @@ import { relations } from "drizzle-orm";
 import { sessionTable } from "./sessions.sql";
 import { ID_SIZE_CONFIG, id, timestamps } from "./utils";
 
-import { pgTable, varchar } from "drizzle-orm/pg-core";
+import { index, pgTable, varchar } from "drizzle-orm/pg-core";
+import { chatMemberTable } from "./chats.sql";
 
-export const userTable = pgTable("users", {
-	id,
-	...timestamps,
-	githubId: varchar({ length: 20 }).notNull().unique(),
-	username: varchar({ length: 39 }).notNull().unique(),
-	email: varchar({ length: 255 }).notNull().unique(),
-});
+export const userTable = pgTable(
+	"users",
+	{
+		id,
+		...timestamps,
+		githubId: varchar({ length: 20 }).notNull().unique(),
+		username: varchar({ length: 39 }).notNull().unique(),
+		email: varchar({ length: 255 }).notNull().unique(),
+	},
+	(table) => [
+		{
+			idIndex: index().on(table.id),
+		},
+	],
+);
 
 export const userTableRelations = relations(userTable, ({ one, many }) => ({
-	profile: one(userProfileTable, {
-		fields: [userTable.id],
-		references: [userProfileTable.userId],
-		relationName: "profile",
-	}),
+	profile: one(userProfileTable),
 	sessions: many(sessionTable),
 	chat: many(chatMemberTable),
 }));
 
-export const userProfileTable = pgTable("user_profiles", {
-	id,
-	...timestamps,
-	userId: varchar(ID_SIZE_CONFIG)
-		.notNull()
+export const userProfileTable = pgTable(
+	"user_profiles",
+	{
+		id,
+		...timestamps,
+		userId: varchar(ID_SIZE_CONFIG)
+			.notNull()
 			.references(() => userTable.id, { onDelete: "cascade" })
 			.unique(),
-	displayName: varchar({ length: 255 }),
-	avatarUrl: varchar({ length: 255 }),
-	htmlUrl: varchar({ length: 255 }),
-});
+		displayName: varchar({ length: 255 }),
+		avatarUrl: varchar({ length: 255 }),
+		htmlUrl: varchar({ length: 255 }),
+	},
+	(table) => [
+		{
+			userIdIndex: index().on(table.userId),
+		},
+	],
+);
 
 export const userProfileTableRelations = relations(
 	userProfileTable,
