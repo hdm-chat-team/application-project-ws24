859cea58d71296fdc230bf807dce2459eb113878
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-12-24 22:55:31 +0100

    test(backend): 💩 fix test?
    Signed-off-by: Deniz Gazitepe <dg102@hdm-stuttgart.de>

--- app/backend/api/app.test.ts
+++ app/backend/api/app.test.ts
@@ -1,18 +1,10 @@
-import { describe, expect, mock, test } from "bun:test";
-import { createMiddleware } from "hono/factory";
+import { describe, expect, test } from "bun:test";
 import { testClient } from "hono/testing";
 import app, { type ApiType } from "./app";
 
-const { api } = testClient(app as ApiType);
-
-mock.module("#/lib/middleware", () => ({
-	limiter: createMiddleware(async (_, next) => {
-		return next();
-	}),
-}));
-
 describe("/api", () => {
 	test("GET /", async () => {
+		const { api } = testClient(app as ApiType);
 		const res = await api.$get();
 
 		expect(await res.text()).toBe("Connected!");
