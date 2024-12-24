2c440f4246235634d145ef7fb6a7d620f9cb9365
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-12-24 19:05:29 +0100

    refactor(backend/chat): ✏️ fix logging when user only connected to one chat
    Signed-off-by: Deniz Gazitepe <dg102@hdm-stuttgart.de>

--- app/backend/api/socket/index.ts
+++ app/backend/api/socket/index.ts
@@ -11,24 +11,26 @@ export const socketRouter = createRouter().get(
 	protectedRoute,
 	upgradeWebSocket(async (c) => {
 		const { id, username } = c.get("user");
-		const userChats = await selectChatIdsByUserId.execute({ id });
+		const chats = await selectChatIdsByUserId.execute({ id });
 		return {
 			onOpen: async (_, ws) => {
 				const socket = ws.raw as ServerWebSocket;
 
 				// * Subscribe to personal and chat channels
 				socket.subscribe(id);
-				for (const { chatId } of userChats) {
+				for (const { chatId } of chats) {
 					socket.subscribe(chatId);
 				}
 
-				console.log(`${username} connected to ${userChats.length} chats`);
+				console.log(
+					`${username} connected to ${chats.length} chat${chats.length > 1 ? "s" : ""}`,
+				);
 			},
 			onClose: (_, ws) => {
 				const socket = ws.raw as ServerWebSocket;
 
 				socket.unsubscribe(id);
-				for (const { chatId } of userChats) {
+				for (const { chatId } of chats) {
 					socket.unsubscribe(chatId);
 				}
 				console.log(`${username} disconnected`);
