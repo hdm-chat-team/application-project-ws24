268d586784174ba227fd7c4981c3a2a23149143e
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-12-26 01:08:53 +0100

    chore: 🙈 version drizzle migrations
    Signed-off-by: Deniz Gazitepe <dg102@hdm-stuttgart.de>

--- app/drizzle.config.ts
+++ app/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 import env from "#env";
 
 export default defineConfig({
-	out: "./drizzle",
+	out: "../drizzle",
 	schema: "./backend/**/*.sql.ts", // ! All schema files end with .sql.ts
 	dialect: "postgresql",
 	casing: "snake_case",
