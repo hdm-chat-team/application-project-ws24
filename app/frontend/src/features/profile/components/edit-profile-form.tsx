78eda3920bd5b2e339952d56e880454a347e476a
basti <bs117@hdm-stuttgart.de>
2024-12-28 15:53:21 +0100

    feat: :art: cleaner import

--- app/frontend/src/features/profile/components/edit-profile-form.tsx
+++ app/frontend/src/features/profile/components/edit-profile-form.tsx
@@ -11,7 +11,7 @@ import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
 import { useForm } from "@tanstack/react-form";
 import { z } from "zod";
-import { Input } from "../../../components/ui/input";
+import { Input } from "@/components/ui/input";
 
 // * min length 2 characters
 
