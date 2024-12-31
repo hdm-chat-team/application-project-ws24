acf4f4d50b903858a163278c2e25ab752702c960
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-12-31 02:26:02 +0100

    refactor(frontend): 🎨 update mutation and queryKeys

--- app/frontend/src/features/chat/hooks/use-save-message.tsx
+++ app/frontend/src/features/chat/hooks/use-save-message.tsx
@@ -7,7 +7,7 @@ export function useSaveMessage(chatId: string) {
 	const queryClient = useQueryClient();
 
 	return useMutation({
-		mutationKey: ["saveMessage"],
+		mutationKey: ["db/save-message", chatId],
 		mutationFn: async (message: Message) => {
 			await db.messages.add(message);
 		},
