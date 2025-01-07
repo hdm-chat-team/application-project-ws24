21324d286809d89186ea4bf652c840e0c2dfbb9b
basti <bs117@hdm-stuttgart.de>
2025-01-07 12:51:11 +0100

    feat: :art: biome formatting

--- app/backend/lib/uploadthing.ts
+++ app/backend/lib/uploadthing.ts
@@ -1,4 +1,4 @@
-import { createUploadthing, type FileRouter } from "uploadthing/server";
+import { type FileRouter, createUploadthing } from "uploadthing/server";
 
 const f = createUploadthing();
 
