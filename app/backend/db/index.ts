5beadd328130d16bf0dd1eb5bb2300fa2deae7c3
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2025-01-08 11:06:06 +0100

    fix(db): 🐛 add messageSchema to query client

--- app/backend/db/index.ts
+++ app/backend/db/index.ts
@@ -1,11 +1,12 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import env from "#env";
 import * as chatSchema from "./chats.sql";
+import * as messageSchema from "./messages.sql";
 import * as sessionSchema from "./sessions.sql";
 import * as userSchema from "./users.sql";
 
 const db = drizzle(env.DATABASE_URL, {
-	schema: { ...userSchema, ...sessionSchema, ...chatSchema },
+	schema: { ...userSchema, ...sessionSchema, ...chatSchema, ...messageSchema },
 	casing: "snake_case",
 });
 
