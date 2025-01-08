562b572a863cbef70daec179b641c66471e881a1
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2025-01-08 16:56:12 +0100

    refactor(backend/socket): ⏪️ back to per user topics

--- app/backend/api/socket/index.ts
+++ app/backend/api/socket/index.ts
@@ -1,7 +1,6 @@
 import type { ServerWebSocket } from "bun";
 import { createBunWebSocket } from "hono/bun";
 import { createRouter } from "#api/factory";
-import { selectChatIdsByUserId } from "#db/chats";
 import { protectedRoute } from "#lib/middleware";
 
 const { upgradeWebSocket } = createBunWebSocket();
@@ -11,28 +10,19 @@ export const socketRouter = createRouter().get(
 	protectedRoute,
 	upgradeWebSocket(async (c) => {
 		const { id, username } = c.get("user");
-		const chats = await selectChatIdsByUserId.execute({ id });
 		return {
 			onOpen: async (_, ws) => {
 				const socket = ws.raw as ServerWebSocket;
 
-				// * Subscribe to personal and chat channels
 				socket.subscribe(id);
-				for (const { chatId } of chats) {
-					socket.subscribe(chatId);
-				}
 
-				console.log(
-					`${username} connected to ${chats.length} chat${chats.length > 1 ? "s" : ""}`,
-				);
+				console.log(`${username} connected`);
 			},
 			onClose: (_, ws) => {
 				const socket = ws.raw as ServerWebSocket;
 
 				socket.unsubscribe(id);
-				for (const { chatId } of chats) {
-					socket.unsubscribe(chatId);
-				}
+				
 				console.log(`${username} disconnected`);
 			},
 		};
