6bfb55dd903ce921f3407cc0cc62d325a337eeae
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2025-01-10 18:16:06 +0100

    style(frontend): 🎨 refactoring

--- app/frontend/src/lib/db.ts
+++ app/frontend/src/lib/db.ts
@@ -3,14 +3,17 @@ import type { Message } from "@server/db/messages";
 import Dexie from "dexie";
 import type { EntityTable } from "dexie";
 
-const db = new Dexie("database") as Dexie & {
+type LocalDatabase = Dexie & {
 	messages: EntityTable<Message, "id">;
 	chats: EntityTable<Chat, "id">;
 };
+
+const db = new Dexie("database") as LocalDatabase;
 
 db.version(9).stores({
 	messages: "id, body, state, chatId, authorId, createdAt, updatedAt",
 	chats: "id, name, createdAt, updatedAt",
 });
 
+export default db;
 export { db };
