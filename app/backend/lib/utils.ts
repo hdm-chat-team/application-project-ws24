ce8e1a50fb5f9ed0430604a7ff2bae338b825425
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2025-01-10 18:18:19 +0100

    feat(backend/socket): 🧑‍💻 typesafe send method

--- app/backend/lib/utils.ts
+++ app/backend/lib/utils.ts
@@ -1,4 +1,5 @@
 import type { Server, ServerWebSocket, ServerWebSocketSendStatus } from "bun";
+import type { WSContext } from "hono/ws";
 import { type WSEventData, wsEventDataSchema } from "#shared/types";
 
 let serverInstance: Server;
@@ -17,9 +18,10 @@ export function getServer() {
 /**
  * Sends a message to subscribers of the topic.
  * @param topic The topic name.
- * @param data The data to be sent. It must conform to WSEventData schema
+ * @param data The data to be sent. It must conform to WSEventData schema.
  * @param socket Optional WebSocket server or socket instance. Defaults to global server instance
  * @param compress Should the data be compressed? If the client does not support compression, this is ignored.
+ * @returns {ServerWebSocketSendStatus} The status of the send operation.
  * @throws ZodError if data doesn't match WSEventData schema
  */
 export function publish(
@@ -27,11 +29,23 @@ export function publish(
 	data: WSEventData,
 	compress?: boolean,
 	socket: ServerWebSocket | Server = getServer(),
+): ServerWebSocketSendStatus {
+	wsEventDataSchema.parse(data);
+	return socket.publish(topic, JSON.stringify(data), compress);
+}
+
+/**
+ * Sends data over a WebSocket connection
+ * @param socket - The WebSocket connection to send data through. (either Bun or Hono WebSocket)
+ * @param data - The data to be sent. It must conform to WSEventData schema.
+ * @param compress - Should the data be compressed? If the client does not support compression, this is ignored.
+ * @throws {ZodError} When the data fails schema validation
+ */
+export function send(
+	socket: ServerWebSocket | WSContext,
+	data: WSEventData,
+	compress?: boolean,
 ) {
 	wsEventDataSchema.parse(data);
-	return socket.publish(
-		topic,
-		JSON.stringify(data),
-		compress,
-	) as ServerWebSocketSendStatus;
+	return socket.send(JSON.stringify(data), compress);
 }
