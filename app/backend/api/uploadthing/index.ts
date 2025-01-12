cd5e03bcd2a59d76098a39479a2f62a2f6a54365
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2025-01-12 21:53:15 +0100

    feat(uploadthing): ✨ custom file names and IDs

--- app/backend/api/uploadthing/index.ts
+++ app/backend/api/uploadthing/index.ts
@@ -1,23 +1,30 @@
+import { createId } from "@application-project-ws24/cuid";
+import { contextStorage, getContext } from "hono/context-storage";
 import {
 	type FileRouter as FR,
+	UTFiles,
 	createRouteHandler,
 	createUploadthing,
 } from "uploadthing/server";
+import type { Env } from "#api/app.env";
 import { createRouter } from "#api/factory";
 import env from "#env";
-import { protectedRoute } from "#lib/middleware";
 
-const f = createUploadthing();
+const routeBuilder = createUploadthing();
 
 export const uploadRouter = {
-	avatar: f({
-		image: {
-			maxFileSize: "4MB",
-			maxFileCount: 1,
-		},
-	}).onUploadComplete((data) => {
-		console.log("upload completed", data);
-	}),
+	avatar: routeBuilder(["image"])
+		.middleware(async ({ files }) => {
+			const { user, session } = getContext<Env>().var;
+			if (!(user && session)) throw new Error("Unauthorized");
+
+			const fileOverrides = files.map((file) => {
+				return { ...file, name: `${user.id}:avatar`, customId: createId() };
+			});
+
+			return { [UTFiles]: fileOverrides };
+		})
+		.onUploadComplete(() => {}),
 } satisfies FR;
 
 const handlers = createRouteHandler({
@@ -27,8 +34,8 @@ const handlers = createRouteHandler({
 	},
 });
 
-export const uploadthingRouter = createRouter().all("/", protectedRoute, (c) =>
-	handlers(c.req.raw),
-);
+export const uploadthingRouter = createRouter()
+	.use(contextStorage())
+	.all("/", (c) => handlers(c.req.raw));
 
 export type FileRouter = typeof uploadRouter;
