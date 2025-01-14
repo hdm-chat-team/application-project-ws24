5981212cf4c176a071f0321554a7e4816f1e185e
Maria Alyssa Di Napoli <alyssa.maria.di@gmail.com>
2025-01-14 01:55:22 +0100

    chore: :art: format

--- app/frontend/src/routes/(app)/_authenticated/user.tsx
+++ app/frontend/src/routes/(app)/_authenticated/user.tsx
@@ -3,14 +3,14 @@ import { EditProfileForm } from "@/features/profile/components";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/(app)/_authenticated/user")({
-  component: ProfilePage,
+	component: ProfilePage,
 });
 
 function ProfilePage() {
-  return (
-    <div>
-      <TopNav />
-      <EditProfileForm />
-    </div>
-  );
+	return (
+		<div>
+			<TopNav />
+			<EditProfileForm />
+		</div>
+	);
 }
