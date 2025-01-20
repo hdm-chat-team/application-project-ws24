5827851fb2a0834c73cae7185b18d1852bcb8689
basti <bs117@hdm-stuttgart.de>
2025-01-20 16:57:04 +0100

    feat: :sparkles: file upload in message form

--- app/frontend/src/features/message/components/message-form.tsx
+++ app/frontend/src/features/message/components/message-form.tsx
@@ -1,22 +1,32 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { usePostMessageMutation } from "@/features/message/hooks";
+import { useUploadThing } from "@/features/uploadthing/hooks";
 import { useForm } from "@tanstack/react-form";
 import { z } from "zod";
 
-const messageFormSchema = z.object({ body: z.string().trim().nonempty() });
+const messageFormSchema = z.object({ body: z.string().trim().nonempty(), file: z.instanceof(File).nullable() });
 
 export default function MessageForm({ chatId }: { chatId: string }) {
 	const postMessageMutation = usePostMessageMutation(chatId);
+	const { startUpload } = useUploadThing("attachment");
 
 	const form = useForm({
 		defaultValues: {
 			body: "",
+			file: null,
 		},
 		onSubmit: async ({ value }) => {
-			await postMessageMutation.mutateAsync(value.body);
-
-			form.reset();
+			try {
+				if (value.file) {
+					await startUpload([value.file]);
+				} else {
+					await postMessageMutation.mutateAsync(value.body);
+				}
+				form.reset();
+			} catch (error) {
+				console.error("Error:", error);
+			}
 		},
 		validators: {
 			onSubmit: messageFormSchema,
