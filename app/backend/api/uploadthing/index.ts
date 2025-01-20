3778e9c12c27604de71ed93177637c6bf53b50c0
basti <bs117@hdm-stuttgart.de>
2025-01-20 13:13:39 +0100

    feat: :sparkles: add message creation and linking with database

--- app/backend/api/uploadthing/index.ts
+++ app/backend/api/uploadthing/index.ts
@@ -2,6 +2,7 @@ import type { Env } from "#api/app.env";
 import { createRouter } from "#api/factory";
 import { insertAttachment } from "#db/attachments";
 import type { attachmentTypeEnum } from "#db/attachments.sql";
+import { insertMessage } from "#db/messages";
 import env from "#env";
 import { createId } from "@application-project-ws24/cuid";
 import { contextStorage, getContext } from "hono/context-storage";
@@ -15,6 +16,8 @@ import type { UploadedFileData } from "uploadthing/types";
 
 interface FileWithMessageId extends UploadedFileData {
 	messageId: string;
+	chatId: string;
+	messageText?: string;
 }
 
 const routeBuilder = createUploadthing();
@@ -48,7 +51,19 @@ export const uploadRouter = {
 			return { [UTFiles]: fileOverrides };
 		})
 		.onUploadComplete(async ({ file }) => {
-			const { messageId } = file as FileWithMessageId;
+			const { user } = getContext<Env>().var;
+			if (!user) throw new Error("Unauthorized");
+
+			const { messageId, chatId, messageText } = file as FileWithMessageId;
+
+			await insertMessage({
+				id: messageId,
+				chatId,
+				authorId: user.id,
+				state: "pending",
+				body: messageText || "",
+				createdAt: new Date().toISOString(),
+			});
 
 			const type: (typeof attachmentTypeEnum.enumValues)[number] =
 				file.type.startsWith("image/")
