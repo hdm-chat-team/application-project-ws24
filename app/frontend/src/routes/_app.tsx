f512d82aefd545ad6ec1d3ae7a11a7d059dc3fb0
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2025-01-20 17:06:01 +0100

    refactor(frontend): 💄 sidebar layout

--- app/frontend/src/routes/_app.tsx
+++ app/frontend/src/routes/_app.tsx
@@ -1,9 +1,14 @@
+import {
+	Sidebar,
+	SidebarInset,
+	SidebarProvider,
+} from "@/components/ui/sidebar";
 import { SocketProvider } from "@/context";
 import { authQueryOptions } from "@/features/auth/queries";
+import { Chat, ChatHeader } from "@/features/chat/components";
+import { ChatProvider } from "@/features/chat/context";
 import { Outlet, createFileRoute, redirect } from "@tanstack/react-router";
-import { Toaster } from "sonner";
 
-// * Layout for authenticated routes
 export const Route = createFileRoute("/_app")({
 	beforeLoad: async ({ context: { queryClient } }) => {
 		if (!(await queryClient.fetchQuery(authQueryOptions)))
@@ -11,8 +16,20 @@ export const Route = createFileRoute("/_app")({
 	},
 	component: () => (
 		<SocketProvider>
-			<Outlet />
-			<Toaster closeButton richColors />
+			<SidebarProvider className="absolute top-0 left-0 h-full min-h-full">
+				<ChatProvider>
+					<Sidebar>
+						{/* Sidebar content defined by routes */}
+						<Outlet />
+					</Sidebar>
+					<SidebarInset className="flex flex-col">
+						<ChatHeader />
+						<main className="flex w-full flex-1 overflow-hidden">
+							<Chat />
+						</main>
+					</SidebarInset>
+				</ChatProvider>
+			</SidebarProvider>
 		</SocketProvider>
 	),
 });
---
+++ app/frontend/src/routes/_app/index.tsx
@@ -0,0 +1,57 @@
+import {
+	SidebarContent,
+	SidebarGroup,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	SidebarSeparator,
+} from "@/components/ui/sidebar";
+
+import { useChat } from "@/features/chat/context";
+import { chatsQueryFn } from "@/features/chat/queries";
+
+import { createFileRoute } from "@tanstack/react-router";
+import { useLiveQuery } from "dexie-react-hooks";
+
+export const Route = createFileRoute("/_app/")({
+	component: () => <ChatListSidebar />,
+});
+
+function ChatListSidebar() {
+	const chats = useLiveQuery(chatsQueryFn, []);
+	const { chat: currentChat, setChatId } = useChat();
+
+	/* 
+	 TODO: create proper chat list item component with avatar, name, last message, etc.
+	 TODO: add sorting by last message time
+	 TODO: add unread message count badge
+	 TODO: add sorting chips (all, unread)
+	 TODO: add searchbar
+	*/
+	return (
+		<>
+			<SidebarHeader>Chats</SidebarHeader>
+			<SidebarSeparator />
+			<SidebarContent>
+				<SidebarGroup>
+					<SidebarMenu>
+						{chats?.map(({ id, name }) => (
+							<SidebarMenuItem key={id}>
+								<div className="flex">
+									<SidebarMenuButton
+										value={id}
+										onClick={(event) => setChatId(event.currentTarget.value)}
+										isActive={id === currentChat?.id}
+									>
+										{name}
+									</SidebarMenuButton>
+								</div>
+							</SidebarMenuItem>
+						))}
+					</SidebarMenu>
+				</SidebarGroup>
+			</SidebarContent>
+		</>
+	);
+}
