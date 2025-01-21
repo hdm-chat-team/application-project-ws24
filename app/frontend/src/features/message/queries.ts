bd256d21827ac81ce370f5cd25e3c7714b2aaa8b
basti <bs117@hdm-stuttgart.de>
2025-01-21 22:22:43 +0100

    feat: :sparkles: added queries to load messages with attachments

--- app/frontend/src/features/message/queries.ts
+++ app/frontend/src/features/message/queries.ts
@@ -1,15 +1,28 @@
 import { db } from "@/lib/db";
 import { queryOptions } from "@tanstack/react-query";
 
-export const messagesByChatIdQueryFn = (id: string) =>
-	db.messages.where("chatId").equals(id).sortBy("createdAt");
+export const messagesByChatIdQueryFn = async (id: string) => {
+	const messages = await db.messages
+		.where("chatId")
+		.equals(id)
+		.sortBy("createdAt");
 
-export const messagesByChatIdQueryOptions = (chatId: string) =>
-	queryOptions({
-		queryKey: ["db/messages-by-chat", chatId],
-		initialData: [],
-		queryFn: () => messagesByChatIdQueryFn(chatId),
-	});
+	const messagesWithAttachments = await Promise.all(
+		messages.map(async (message) => {
+			const attachments = await db.attachments
+				.where("messageId")
+				.equals(message.id)
+				.toArray();
+
+			return {
+				...message,
+				attachments,
+			};
+		}),
+	);
+
+	return messagesWithAttachments;
+};
 
 export const messageStateByIdQueryFn = (id: string) =>
 	db.messages
@@ -17,3 +30,10 @@ export const messageStateByIdQueryFn = (id: string) =>
 		.equals(id)
 		.first()
 		.then((message) => message?.state);
+
+export const messagesByChatIdQueryOptions = (chatId: string) =>
+	queryOptions({
+		queryKey: ["db/messages-by-chat", chatId],
+		initialData: [],
+		queryFn: () => messagesByChatIdQueryFn(chatId),
+	});
