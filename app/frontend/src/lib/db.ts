e26222cca7d4377259a78fefa1bf93c0d290da7e
basti <bs117@hdm-stuttgart.de>
2025-01-21 19:18:14 +0100

    feat: :sparkles: updated localDB for Attachments

--- app/frontend/src/lib/db.ts
+++ app/frontend/src/lib/db.ts
@@ -1,18 +1,21 @@
 import type { Chat } from "@server/db/chats";
 import type { Message } from "@server/db/messages";
+import type { Attachment } from "@server/db/attachments"; 
 import Dexie from "dexie";
 import type { EntityTable } from "dexie";
 
 export type LocalDatabase = Dexie & {
 	messages: EntityTable<Message, "id">;
 	chats: EntityTable<Chat, "id">;
+	attachments: EntityTable<Attachment>;
 };
 
 const db = new Dexie("database") as LocalDatabase;
 
-db.version(9).stores({
+db.version(10).stores({
 	messages: "id, body, state, chatId, authorId, createdAt, updatedAt",
 	chats: "id, name, createdAt, updatedAt",
+	attachments: "id, messageId, url, type",
 });
 
 export default db;
