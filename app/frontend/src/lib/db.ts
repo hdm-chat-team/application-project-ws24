7f4c45aeb33d9d2945ec8d8d4d40a0b0c5459b50
basti <bs117@hdm-stuttgart.de>
2025-01-21 21:45:45 +0100

    feat: :art: update localDB

--- app/frontend/src/lib/db.ts
+++ app/frontend/src/lib/db.ts
@@ -7,15 +7,15 @@ import type { EntityTable } from "dexie";
 export type LocalDatabase = Dexie & {
 	messages: EntityTable<Message, "id">;
 	chats: EntityTable<Chat, "id">;
-	attachments: EntityTable<Attachment>;
+	attachments: EntityTable<Attachment, "url">;
 };
 
 const db = new Dexie("database") as LocalDatabase;
 
-db.version(10).stores({
+db.version(11).stores({
 	messages: "id, body, state, chatId, authorId, createdAt, updatedAt",
 	chats: "id, name, createdAt, updatedAt",
-	attachments: "id, messageId, url, type",
+	attachments: "url, messageId, url, type",
 });
 
 export default db;
