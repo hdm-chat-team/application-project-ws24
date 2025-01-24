a8bb228b7fca4f944ec3c4eae90ce74dd21508cc
basti <bs117@hdm-stuttgart.de>
2025-01-24 12:53:04 +0100

    feat: :fire: update attatments db

--- app/frontend/src/lib/db.ts
+++ app/frontend/src/lib/db.ts
@@ -12,10 +12,10 @@ export type LocalDatabase = Dexie & {
 
 const db = new Dexie("database") as LocalDatabase;
 
-db.version(11).stores({
+db.version(12).stores({
 	messages: "id, body, state, chatId, authorId, createdAt, updatedAt",
 	chats: "id, name, createdAt, updatedAt",
-	attachments: "url, messageId, url, type",
+	attachments: "url, messageId, type",
 });
 
 export default db;
