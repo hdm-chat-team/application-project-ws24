73792a17fde04d7091503b40fa17cd4a14c64c22
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2025-01-25 22:36:56 +0100

    feat(frontend): 🚸 optimistic update on avatar uploader

---
+++ app/frontend/src/features/profile/hooks/use-upload-avatar.tsx
@@ -0,0 +1,52 @@
+import { authQueryOptions } from "@/features/auth/queries";
+import { useUploadThing } from "@/features/uploadthing/hooks";
+import api from "@/lib/api";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+export function useUploadUserAvatar() {
+	const queryClient = useQueryClient();
+	const { startUpload } = useUploadThing(
+		(routeRegistry) => routeRegistry.avatar,
+	);
+	return useMutation({
+		mutationKey: [api.uploadthing.$url().pathname, "avatar"],
+		mutationFn: async (files: File[]) => {
+			const result = await startUpload(files);
+			return result;
+		},
+		onMutate: async ([variables]) => {
+			await queryClient.cancelQueries(authQueryOptions);
+			const previousUserData = queryClient.getQueryData(
+				authQueryOptions.queryKey,
+			);
+			if (!previousUserData) throw new Error("No previous data found");
+			const objectUrl = URL.createObjectURL(variables);
+			queryClient.setQueryData(
+				authQueryOptions.queryKey,
+				(data) =>
+					data && {
+						user: data.user,
+						profile: {
+							...data.profile,
+							avatarUrl: objectUrl,
+						},
+					},
+			);
+			return { previousUserData, objectUrl };
+		},
+		onError: (_error, _variables, context) => {
+			if (context) {
+				queryClient.setQueryData(
+					authQueryOptions.queryKey,
+					context.previousUserData,
+				);
+			}
+		},
+		onSettled: (_data, _error, _variables, context) => {
+			if (context?.objectUrl) {
+				URL.revokeObjectURL(context.objectUrl);
+			}
+			queryClient.invalidateQueries(authQueryOptions);
+		},
+	});
+}
