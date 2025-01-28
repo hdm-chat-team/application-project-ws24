b7cfddbdcc186bb2cadb7a7e9faa9ae628f51c64
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2025-01-28 14:35:45 +0100

    refactor(frontend/message): 🎨 optimize query

--- app/frontend/src/features/message/queries.ts
+++ app/frontend/src/features/message/queries.ts
@@ -4,11 +4,7 @@ export const messagesByChatIdQueryFn = async (id: string) =>
 	db.messages.where("chatId").equals(id).sortBy("createdAt");
 
 export const messageStateByIdQueryFn = (id: string) =>
-	db.messages
-		.where("id")
-		.equals(id)
-		.first()
-		.then((message) => message?.state);
+	db.messages.get(id).then((message) => message?.state);
 
 export const attachmentsByMessageIdQueryFn = (id: string) =>
 	db.attachments.where({ messageId: id }).toArray();
