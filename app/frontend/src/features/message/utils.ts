2f5476eb6f652c5886ae209ea7d88ced8402b880
basti <bs117@hdm-stuttgart.de>
2025-01-29 16:21:57 +0100

    feat: :sparkles: add local timezone for message timestamps

--- app/frontend/src/features/message/utils.ts
+++ app/frontend/src/features/message/utils.ts
@@ -1,5 +1,11 @@
 import { createId } from "@application-project-ws24/cuid";
+import { Temporal } from "@js-temporal/polyfill";
 import type { Message } from "@server/db/messages";
+
+// * extends server message with receivedAt
+export interface LocalMessage extends Message {
+	receivedAt: string;
+}
 
 export function createMessage(
 	chatId: string,
@@ -14,5 +20,6 @@ export function createMessage(
 		body,
 		authorId,
 		chatId,
-	};
+		receivedAt: Temporal.Now.zonedDateTimeISO("Europe/Berlin").toString(),
+	} as LocalMessage;
 }
