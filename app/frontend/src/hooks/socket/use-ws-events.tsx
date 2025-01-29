b077c12997c74aeb7d047f727cd17a84e2bfe234
basti <bs117@hdm-stuttgart.de>
2025-01-29 18:23:11 +0100

    feat: :sparkles: added formatBerlinTime for message sync and message_incoming so those messages will get a retrievetat too with berlin time

--- app/frontend/src/hooks/socket/use-ws-events.tsx
+++ app/frontend/src/hooks/socket/use-ws-events.tsx
@@ -9,6 +9,7 @@ import {
 	useSaveMessageBatch,
 	useUpdateMessage,
 } from "@/features/message/hooks";
+import { formatBerlinTime } from "@/features/message/utils";
 import { type WSEventData, wsEventDataSchema } from "@shared/types";
 import { useQueryClient } from "@tanstack/react-query";
 import { useCallback } from "react";
@@ -34,19 +35,26 @@ export function useWebSocketEvents(sendMessage: (data: WSEventData) => void) {
 
 			switch (data.type) {
 				case "message_sync": {
-					saveMessagesByChat(data.payload);
+					const localMessages = data.payload.map((message) => ({
+						...message,
+						receivedAt: formatBerlinTime(),
+					}));
+					saveMessagesByChat(localMessages);
 					break;
 				}
 				case "message_incoming": {
-					const message = data.payload;
-					saveMessage(message);
+					const localMessage = {
+						...data.payload,
+						receivedAt: formatBerlinTime(),
+					};
+					saveMessage(localMessage);
 					sendMessage({
 						type: "message_received",
-						payload: { id: message.id, authorId: message.authorId },
+						payload: { id: localMessage.id, authorId: localMessage.authorId },
 					});
 
 					const chatSynched = queryClient.getQueryData(
-						chatByIdQueryOptions(message.chatId).queryKey,
+						chatByIdQueryOptions(localMessage.chatId).queryKey,
 					);
 					if (!chatSynched) queryClient.refetchQueries(syncChatsQueryOptions);
 					break;
