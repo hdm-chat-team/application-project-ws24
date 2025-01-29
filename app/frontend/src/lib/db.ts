bd363a02c05f67c6083807e0a40dcdac1b030b99
basti <bs117@hdm-stuttgart.de>
2025-01-29 15:55:27 +0100

    feat: :sparkles: added js-temporal/polyfill and updated dexie table

--- app/frontend/src/lib/db.ts
+++ app/frontend/src/lib/db.ts
@@ -12,8 +12,8 @@ export type LocalDatabase = Dexie & {
 
 const db = new Dexie("database") as LocalDatabase;
 
-db.version(12).stores({
-	messages: "id, body, state, chatId, authorId, createdAt, updatedAt",
+db.version(13).stores({
+	messages: "id, body, state, chatId, authorId, createdAt, updatedAt, receivedAt",
 	chats: "id, name, createdAt, updatedAt",
 	attachments: "url, messageId, type",
 });
