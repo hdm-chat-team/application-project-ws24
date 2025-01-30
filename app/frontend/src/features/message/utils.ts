01f7774ea8ab59e6c906805be892b89861d0862b
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2025-01-30 18:05:40 +0100

    84 add timestamp display to message (#85)
    * feat: :sparkles: added js-temporal/polyfill and updated dexie table
    
    * feat: :art: boime fix
    
    * feat: :sparkles: add local timezone for message timestamps
    
    * feat: :art: formatBerlinTime helper function
    
    * feat: :bug: changed EntityTable to LocalMessage , so local db has to know to save receivedAt field
    
    * feat: :bug: MessageBubbleProps to LocalMessage because he has to show the  receivedAt
    
    * feat: :bug: added localmessage to all db operations
    
    * feat: :sparkles: added formatBerlinTime for message sync and message_incoming so those messages will get a retrievetat too with berlin time
    
    * chore: 📌 lockfile
    
    ---------
    
    Co-authored-by: basti <bs117@hdm-stuttgart.de>

--- app/frontend/src/features/message/utils.ts
+++ app/frontend/src/features/message/utils.ts
@@ -1,11 +1,28 @@
 import { createId } from "@application-project-ws24/cuid";
+import { Temporal } from "@js-temporal/polyfill";
 import type { Message } from "@server/db/messages";
+
+// * extends server message with receivedAt
+export interface LocalMessage extends Message {
+	receivedAt: string;
+}
+
+export function formatBerlinTime() {
+	return Temporal.Now.zonedDateTimeISO("Europe/Berlin").toLocaleString(
+		"de-DE",
+		{
+			hour: "2-digit",
+			minute: "2-digit",
+			hour12: false,
+		},
+	);
+}
 
 export function createMessage(
 	chatId: string,
 	authorId: string,
 	body: string,
-): Message {
+): LocalMessage {
 	return {
 		id: createId(),
 		createdAt: new Date().toISOString(),
@@ -14,5 +31,6 @@ export function createMessage(
 		body,
 		authorId,
 		chatId,
+		receivedAt: formatBerlinTime(),
 	};
 }
--- app/frontend/src/lib/db.ts
+++ app/frontend/src/lib/db.ts
@@ -1,19 +1,20 @@
+import type { LocalMessage } from "@/features/message/utils";
 import type { Attachment } from "@server/db/attachments";
 import type { Chat } from "@server/db/chats";
-import type { Message } from "@server/db/messages";
 import Dexie from "dexie";
 import type { EntityTable } from "dexie";
 
 export type LocalDatabase = Dexie & {
-	messages: EntityTable<Message, "id">;
+	messages: EntityTable<LocalMessage, "id">;
 	chats: EntityTable<Chat, "id">;
 	attachments: EntityTable<Attachment & { blob?: Blob }, "url">;
 };
 
 const db = new Dexie("database") as LocalDatabase;
 
-db.version(12).stores({
-	messages: "id, body, state, chatId, authorId, createdAt, updatedAt",
+db.version(13).stores({
+	messages:
+		"id, body, state, chatId, authorId, createdAt, updatedAt, receivedAt",
 	chats: "id, name, createdAt, updatedAt",
 	attachments: "url, messageId, type",
 });
