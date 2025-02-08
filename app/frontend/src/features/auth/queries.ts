fb743c8bc5245f84ca5fbb0b17a4a28e4247e526
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2025-02-08 21:46:47 +0100

    refactor(frontend): 🎨 auth query

--- app/frontend/src/features/auth/queries.ts
+++ app/frontend/src/features/auth/queries.ts
@@ -5,6 +5,7 @@ export const authQueryOptions = queryOptions({
 	queryKey: ["GET", api.auth.$url().pathname],
 	queryFn: async () => {
 		const response = await api.auth.$get();
+		if (response.status === 401) return null;
 		return (await response.json()).data;
 	},
 	staleTime: Number.POSITIVE_INFINITY,
