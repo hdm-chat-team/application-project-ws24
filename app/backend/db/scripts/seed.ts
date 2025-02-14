41f858ada2ab972bfcee3f836793ce5794287fa0
basti <bs117@hdm-stuttgart.de>
2025-02-14 19:02:23 +0100

    feat: :zap: using drizzle seed

--- app/backend/db/scripts/seed.ts
+++ app/backend/db/scripts/seed.ts
@@ -1,21 +1,33 @@
 import { createId } from "@application-project-ws24/cuid";
-import { faker } from "@faker-js/faker/locale/de";
+import { seed } from "drizzle-seed";
 import { db } from "#db";
 import { userTable } from "../users.sql";
 
 // run with "bun:db seed" in app folder
-const userIdPool = Array.from({ length: 1000 }, () => createId());
+async function main() {
+	try {
+		const userIdPool = Array.from({ length: 1000 }, () => createId());
 
-try {
-	await db.insert(userTable).values(
-		userIdPool.map((id) => ({
-			id,
-			githubId: Math.floor(100000000 + Math.random() * 900000000).toString(),
-			username: faker.person.fullName().replace(/\s+/g, "").toLowerCase(),
-			email: faker.internet.email().toLowerCase(),
-		})),
-	);
-	console.log("Successfully seeded users");
-} catch (error) {
-	console.error("Seeding failed", error);
+		await seed(db, { userTable }).refine((f) => ({
+			userTable: {
+				count: 1000,
+				columns: {
+					id: f.valuesFromArray({ values: userIdPool, isUnique: true }),
+					githubId: f.int({
+						minValue: 10000000,
+						maxValue: 99999999,
+						isUnique: true,
+					}),
+					username: f.fullName(),
+					email: f.email(),
+				},
+			},
+		}));
+
+		console.log("Successfully seeded users");
+	} catch (error) {
+		console.error("Seeding failed", error);
+	}
 }
+
+main();
