1f450565682e6ea888db74079cb62f02974c9a0c
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2025-02-18 00:36:36 +0100

    perf(backend/db): ⚡️ improve seeding script performance and seed profiles

--- app/backend/db/scripts/seed.ts
+++ app/backend/db/scripts/seed.ts
@@ -1,33 +1,80 @@
 import { createId } from "@application-project-ws24/cuid";
+import { drizzle } from "drizzle-orm/bun-sql";
 import { seed } from "drizzle-seed";
-import { db } from "#db";
-import { userTable } from "../users.sql";
+import { schema } from "#db";
+import env from "#env";
 
-// run with "bun db:seed" in app folder
-async function main() {
-	try {
-		const userIdPool = Array.from({ length: 1000 }, () => createId());
+/* 
+* Number of users to seed
+? should this be a command line argument? 
+*/
+const USER_COUNT = 1000;
 
-		await seed(db, { userTable }).refine((f) => ({
-			userTable: {
-				count: 1000,
-				columns: {
-					id: f.valuesFromArray({ values: userIdPool, isUnique: true }),
-					githubId: f.int({
-						minValue: 10000000,
-						maxValue: 99999999,
-						isUnique: true,
-					}),
-					username: f.fullName(),
-					email: f.email(),
-				},
+// * Tables to seed
+const { userTable, userProfileTable } = schema;
+
+console.log("🌱 Seeding the database...");
+
+try {
+	const GITHUB_ID_RANGE = {
+		start: 10000000,
+		end: 99999999,
+	};
+	await seed(
+		// Bun SQL instance for performance (~10.5s -> ~0.5s)
+		drizzle(env.DATABASE_URL, {
+			schema,
+			casing: "snake_case",
+		}),
+		{
+			userTable,
+			userProfileTable,
+		},
+	).refine((generators) => ({
+		userTable: {
+			count: USER_COUNT,
+			columns: {
+				id: generators.valuesFromArray({
+					values: Array.from({ length: USER_COUNT }, () => createId()),
+					isUnique: true,
+				}),
+				githubId: generators.int({
+					minValue: GITHUB_ID_RANGE.start,
+					maxValue: GITHUB_ID_RANGE.end,
+					isUnique: true,
+				}),
+				username: generators.fullName(),
+				email: generators.email(),
 			},
-		}));
+			with: {
+				userProfileTable: 1,
+			},
+		},
+		userProfileTable: {
+			count: 0,
+			columns: {
+				id: generators.valuesFromArray({
+					values: Array.from({ length: USER_COUNT }, () => createId()),
+					isUnique: true,
+				}),
+				avatarUrl: generators.valuesFromArray({
+					values: Array.from({ length: USER_COUNT }, () => {
+						// random github avatar urls, might be fun to see...
+						const githubUserId =
+							Math.floor(
+								Math.random() *
+									(GITHUB_ID_RANGE.end - GITHUB_ID_RANGE.start + 1),
+							) + GITHUB_ID_RANGE.start;
 
-		console.log("Successfully seeded users");
-	} catch (error) {
-		console.error("Seeding failed", error);
-	}
+						return `https://avatars.githubusercontent.com/u/${githubUserId}`;
+					}),
+				}),
+			},
+		},
+	}));
+} catch (error) {
+	console.log("❌ Seeding failed:");
+	console.error(error);
 }
 
-main();
+console.log("✅ Done!");
