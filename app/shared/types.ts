bf791961fa14a0fc4df251409c3123d97e2d1d25
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2025-02-18 02:46:09 +0100

    chore: 🏷️ add descriptions to ws events

--- app/shared/types.ts
+++ app/shared/types.ts
@@ -17,48 +17,55 @@ const messageAttachmentSchema = z.object({
 	type: z.string().nonempty(),
 });
 
-const wsEventDataTypeSchema = z.enum([
-	"message_sync",
-	"message_incoming",
-	"message_attachment",
-	"message_received",
-	"message_delivered",
-	"message_read",
-	"message_completed",
-]);
-type WSEventDataType = z.infer<typeof wsEventDataTypeSchema>;
-
 const wsEventDataSchema = z.discriminatedUnion("type", [
-	z.object({
-		type: z.literal(wsEventDataTypeSchema.enum.message_sync),
-		payload: z.array(messageSchema),
-	}),
-	z.object({
-		type: z.literal(wsEventDataTypeSchema.enum.message_incoming),
-		payload: messageSchema,
-	}),
-	z.object({
-		type: z.literal(wsEventDataTypeSchema.enum.message_attachment),
-		payload: messageAttachmentSchema,
-	}),
-	z.object({
-		type: z.literal(wsEventDataTypeSchema.enum.message_received),
-		payload: messageSchema.pick({ id: true, authorId: true }),
-	}),
-	z.object({
-		type: z.literal(wsEventDataTypeSchema.enum.message_delivered),
-		payload: cuidSchema,
-	}),
-	z.object({
-		type: z.literal(wsEventDataTypeSchema.enum.message_read),
-		payload: messageSchema.pick({ id: true, authorId: true }),
-	}),
-	z.object({
-		type: z.literal(wsEventDataTypeSchema.enum.message_completed),
-		payload: cuidSchema,
-	}),
+	z
+		.object({
+			type: z.literal("message_sync"),
+			payload: z.array(messageSchema),
+		})
+		.describe("The client receives unsynced messages from the server."),
+	z
+		.object({
+			type: z.literal("message_incoming"),
+			payload: messageSchema,
+		})
+		.describe("The client receives a new message from the server."),
+	z
+		.object({
+			type: z.literal("message_attachment"),
+			payload: messageAttachmentSchema,
+		})
+		.describe("The client receives a new message attachment from the server."),
+	z
+		.object({
+			type: z.literal("message_received"),
+			payload: messageSchema.pick({ id: true, authorId: true }),
+		})
+		.describe("The client receives message from the server."),
+	z
+		.object({
+			type: z.literal("message_delivered"),
+			payload: cuidSchema,
+		})
+		.describe(
+			"The client sends a message delivery confirmation to the server.",
+		),
+	z
+		.object({
+			type: z.literal("message_read"),
+			payload: messageSchema.pick({ id: true, authorId: true }),
+		})
+		.describe("The client sends a message read confirmation to the server."),
+	z
+		.object({
+			type: z.literal("message_completed"),
+			payload: cuidSchema,
+		})
+		.describe(
+			"The client receives a message completion(read by all recipients) confirmation from the server.",
+		),
 ]);
 type WSEventData = z.infer<typeof wsEventDataSchema>;
 
-export { /* messageSchema, */ wsEventDataTypeSchema, wsEventDataSchema };
-export type { WSEventDataType, WSEventData };
+export { wsEventDataSchema };
+export type { WSEventData };
