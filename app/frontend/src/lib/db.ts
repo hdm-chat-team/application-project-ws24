a21a0599ebdd12457029f4c32bedc19a2fa7322b
basti <bs117@hdm-stuttgart.de>
2025-02-22 21:20:02 +0100

    feat: :card_file_box: creating 1 to 1 files table

--- app/frontend/src/lib/db.ts
+++ app/frontend/src/lib/db.ts
@@ -3,20 +3,22 @@ import type { Attachment } from "@server/db/attachments";
 import type { Chat } from "@server/db/chats";
 import Dexie from "dexie";
 import type { EntityTable } from "dexie";
+import type { LocalFile } from "@/features/uploadthing/types";
 
 export type LocalDatabase = Dexie & {
 	messages: EntityTable<LocalMessage, "id">;
 	chats: EntityTable<Chat, "id">;
 	attachments: EntityTable<Attachment & { blob?: Blob }, "url">;
+	files: EntityTable<LocalFile , "customId">;
 };
 
 const db = new Dexie("database") as LocalDatabase;
 
-db.version(13).stores({
+db.version(14).stores({
 	messages:
 		"id, body, state, chatId, authorId, createdAt, updatedAt, receivedAt",
 	chats: "id, name, createdAt, updatedAt",
-	attachments: "url, messageId, type",
+	files: "customId, originalName, type, createdAt",
 });
 
 export default db;
