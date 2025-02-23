3a4ec16005cdfedce0f90616640f24d4c8ca9df2
basti <bs117@hdm-stuttgart.de>
2025-02-23 16:32:10 +0100

    feat: :fire: remove attachments EntityTable because its not needed anymore

--- app/frontend/src/lib/db.ts
+++ app/frontend/src/lib/db.ts
@@ -1,5 +1,4 @@
 import type { LocalMessage } from "@/features/message/utils";
-import type { Attachment } from "@server/db/attachments";
 import type { Chat } from "@server/db/chats";
 import Dexie from "dexie";
 import type { EntityTable } from "dexie";
@@ -8,8 +7,7 @@ import type { LocalFile } from "@/features/uploadthing/types";
 export type LocalDatabase = Dexie & {
 	messages: EntityTable<LocalMessage, "id">;
 	chats: EntityTable<Chat, "id">;
-	attachments: EntityTable<Attachment & { blob?: Blob }, "url">;
-	files: EntityTable<LocalFile , "customId">;
+	files: EntityTable<LocalFile & { blob?: Blob }, "customId">;
 };
 
 const db = new Dexie("database") as LocalDatabase;
