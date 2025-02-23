2248f32a7784f883bb3b5a3346559305182cb7fb
basti <bs117@hdm-stuttgart.de>
2025-02-23 19:56:55 +0100

    feat: :rewind: revert db metas

--- drizzle/0003_rare_rocket_raccoon.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "message_attachments" (
-	"url" varchar(255) PRIMARY KEY NOT NULL,
-	"type" text NOT NULL,
-	"message_id" varchar(15) NOT NULL
-);
---> statement-breakpoint
-ALTER TABLE "messages" ADD COLUMN "has_file" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "message_attachments" ADD CONSTRAINT "message_attachments_message_id_messages_id_fk" FOREIGN KEY ("message_id") REFERENCES "public"."messages"("id") ON DELETE cascade ON UPDATE no action;
