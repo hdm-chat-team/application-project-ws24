f2008e86a688a1442329edb73e4e08f68b0b9ffe
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2025-02-24 13:38:03 +0100

    fix(user): 🦺 search firing on empty search

--- app/frontend/src/features/contacts/queries.ts
+++ app/frontend/src/features/contacts/queries.ts
@@ -1,28 +1,17 @@
 import api from "@/lib/api";
 import { db } from "@/lib/db";
 import type { UserWithProfile } from "@server/db/users";
-import { type UserSearchQuery, userSearchQuerySchema } from "@shared/types";
+import type { UserSearchQuery } from "@shared/types";
 import { queryOptions } from "@tanstack/react-query";
 
 export const contactsQueryFn = () =>
 	db.users.where("relation").equals("contact").toArray();
 
-export const syncContactsQueryOptions = queryOptions({
-	queryKey: ["GET", api.user.contacts.$url().pathname],
-	queryFn: async () => {
-		const response = await api.user.contacts.$get();
-		if (!response.ok) throw new Error("Failed to fetch contacts");
-
-		return (await response.json()).data;
-	},
-});
-
 export const searchUsersQueryOptions = (query: UserSearchQuery) =>
 	queryOptions<UserWithProfile[]>({
 		queryKey: ["GET", api.user.search.$url().pathname, query.search],
 		queryFn: async () => {
-			const { error } = userSearchQuerySchema.safeParse(query);
-			if (error) return [];
+			if (!query.search) return [];
 			const localResults = await userSearchQueryFn(query);
 
 			if (localResults.length > 0) return localResults;
